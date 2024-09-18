4be66a7c8cc93381bc5b94f0bceb4e7b3477e877
Jeff Chen <cytastorm@gmail.com>
2024-09-18 00:16:07 -0400

    added interaction detection

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	[SerializeField] private float radius = 3f;
+	[SerializeField] private Transform interactionTransform;
+	private bool isFocus = false;
+	private bool hasInteracted = false;
+	public virtual void Interact()
+	{
+		Debug.Log("Interacting with " + transform.name);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isFocus && !hasInteracted) 
+		{
+			float distance = Vector3.Distance(
+				PlayerController.PlayerControl.gameObject.transform.position,
+				interactionTransform.position);
+			if (distance <= radius)
+			{
+				Debug.Log("INTERACT");
+				hasInteracted = true;
+			}
+		}
+	}
+
+	public void OnDefocused()
+	{
+		isFocus = false;
+		hasInteracted = false;
+	}
+
+	public void OnFocused()
+	{
+		isFocus = true;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(interactionTransform.position, radius);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private PlayerInput playerInput; 
+
+	[SerializeField] private Camera cam;
+	[SerializeField] private NavMeshAgent agent;
+	[SerializeField] private int raycastRange = 100;
+	[SerializeField] private LayerMask movementMask;
+	[SerializeField] private LayerMask interactableMask;
+
+	private Interactable focus;
+
+	//Singleton stuff
+	public static PlayerController PlayerControl
+	{
+		get; private set;
+	}
+
+	private void Awake()
+	{
+		if (PlayerControl != null && PlayerControl != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			PlayerControl = this;
+		}
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void OnMoveInteract()
+	{
+		Vector3 mousePos = Mouse.current.position.ReadValue();
+		Ray ray = cam.ScreenPointToRay(mousePos);
+		RaycastHit hit;
+
+		// if ray hits interactable
+		if (Physics.Raycast(ray, out hit, raycastRange, interactableMask))
+		{
+			Interactable interactable = hit.collider.GetComponent<Interactable>();
+			if (interactable != null)
+			{
+				SetFocus(interactable);
+				agent.SetDestination(hit.point);
+			}
+		}
+
+		// if the ray hits something walkable move the player towards it
+		else if (Physics.Raycast(ray, out hit, raycastRange, movementMask))
+		{
+			agent.SetDestination(hit.point);
+		}
+	}
+
+	private void SetFocus(Interactable newFocus)
+	{
+		if (newFocus == focus) return;
+
+		//New focus object
+		if (focus != null) focus.OnDefocused();
+		focus = newFocus;
+		newFocus.OnFocused();
+	}
+	
+	private void RemoveFocus()
+	{
+		if (focus != null) focus.OnDefocused();
+		focus = null;
+	}
+}
