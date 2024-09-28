59a113d7543c3ddbe4e02804b434dbc0b6eb5eb2
Xander <xyushchak@gmail.com>
2024-09-28 14:12:52 -0400

    Finished FileIO Node/Parser scripts

---
+++ Assets/Scripts/DialogueTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTester : MonoBehaviour
+{
+    void Start()
+    {
+        TestDialogueParsing();
+    }
+
+    void TestDialogueParsing()
+    {
+        string filePath = Application.dataPath + "/Scripts/Story.twee";
+        DialogueTreeParser.ParseFile(filePath);
+    }
+}
