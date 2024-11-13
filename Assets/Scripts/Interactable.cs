96b0a28356f0162f7aab9ba38b335d83609b3e59
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2024-11-12 19:37:56 -0500

    no. of interactions and movement are customizable

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,9 +6,11 @@ using UnityEngine.AI;  // Import the AI Navigation namespace
 public class Interactable : MonoBehaviour
 {
     [SerializeField] protected float radius = 3f;           
-    [SerializeField] protected Transform interactionTransform;  
-    [SerializeField] protected Vector3 destinationPosition;  
+    [SerializeField] protected Transform interactionTransform;
+    [SerializeField] protected Vector3 destinationPosition;
 
+    public bool hasLimitedInteractions = true;
+    public bool canMove = true;
 
     protected bool isFocus = false;       
     protected bool hasInteracted = false; 
@@ -28,13 +30,19 @@ public class Interactable : MonoBehaviour
     public virtual void Interact()
     {
         Debug.Log("Interacting with " + transform.name);
-        MoveToPosition(destinationPosition);
+
+        // Only move if it is a moving interactable
+        if (canMove)
+        {
+            MoveToPosition(destinationPosition);
+        }        
     }
 
     // Moves the NPC to the destination
     protected void MoveToPosition(Vector3 destination)
     {
-        if (agent != null)
+        // Only move if the interactable has an agent AND a destination
+        if ((agent != null) && (destination != null))
         {
             agent.SetDestination(destination); // Sets the destination for the NavMeshAgent
             isMoving = true; // Sets the NPC to moving state
