dde1c3a851ab68d0d2142f7467576e3e8b4a1c37
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2024-12-13 19:51:35 -0500

    created snow effect

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Camera cam;
+    public Vector3 offset;
+
+    private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.transform.position + offset;
+    }
+}
