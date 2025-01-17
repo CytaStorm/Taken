6c43148dc0e1da450d4f2dec36243199837d17d4
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-01-16 20:58:13 -0500

    created sceneChanger script

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] string flagName;
+    [SerializeField] string destinationScene;
+    [SerializeField] float delayTime;
+    [SerializeField] SceneTwoManager sceneManager;
+
+    private bool sceneChangeActive = false;
+    private float sceneChangeTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Search SceneTwoManager for matching flag
+        foreach (DialogueFlag flag in sceneManager.dialogueFlags)
+        {
+            if ((flag.Name == flagName) && (flag.IsTrue))
+            {
+                sceneChangeActive = true;
+            }
+        }
+
+        if (sceneChangeActive)
+        {
+            sceneChangeTimer += Time.deltaTime;
+
+            if (sceneChangeTimer > delayTime)
+            {
+                SceneManager.LoadScene(destinationScene);
+            }
+        }
+    }
+}
