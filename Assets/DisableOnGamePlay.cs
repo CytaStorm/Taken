60500ab6a4d8c884b96f3e66096227aabf38c77a
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-01-29 19:50:57 -0500

    portrait now gets disabled during gameplay

--- Assets/DisableOnGamePlay.cs
+++ Assets/DisableOnGamePlay.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class DisableOnGamePlay : MonoBehaviour
 {
+    [SerializeField] List<GameObject> toDisable;
+    private UIManager uIManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uIManager = GetComponent<UIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(uIManager.CurrentUIMode == UIMode.Gameplay)
+        {
+            foreach(GameObject gObject in toDisable)
+            {
+                gObject.SetActive(false);
+            }
+            
+        }
+        else if (uIManager.CurrentUIMode == UIMode.Dialogue)
+        {
+            foreach (GameObject gObject in toDisable)
+            {
+                gObject.SetActive(true);
+            }
+        }
     }
 }
