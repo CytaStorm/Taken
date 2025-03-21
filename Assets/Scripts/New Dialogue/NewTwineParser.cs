99fee6a1bd25037e5ded4c44c1df8ac7e96c8104
Jeff Chen (RIT Student) <jc7117@rit.edu>
2025-03-20 20:48:08 -0400

    BORKEN CODE ToT

--- Assets/Scripts/New Dialogue/NewTwineParser.cs
+++ Assets/Scripts/New Dialogue/NewTwineParser.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NewTwineParser
@@ -44,10 +45,30 @@ public class NewTwineParser
 	private static void ParseLinkConditions(NewDialogueNode currentNode)
 	{
 		//Get ifs and their hooks, these should be the same length
-		List<string> ifs = GetRemovedSpecialText(currentNode, "\n(if:", ")");
-		List<string> hooks = GetRemovedSpecialText(currentNode, "[", "]");
-		
-		//parse hooks first
+
+		//If
+		List<string> ifs = GetRemovedSpecialText(currentNode, "\n(if: ", ")");
+		List<NewDialogueFlag> flags = ExtractFlags(ifs);
+
+		//Hooks
+		List<string> unparsedLinks = GetRemovedSpecialText(currentNode, "[ ", " ]");
+		List<List<string>> hooks = new List<List<string>>();
+		for (int i = 0; i < unparsedLinks.Count; i++)
+		{
+			string unparsedLink = unparsedLinks[i];
+			//each hook
+			hooks.Add(GetRemovedSpecialText(unparsedLink, "[[", "]]"));
+		}
+
+		List<(NewDialogueFlag, List<NewDialogueLink>)> conditionals =
+			new List<(NewDialogueFlag, List<NewDialogueLink>)>();
+
+		//UNDER CONSTRUCTION, PARSE HOOKS INTO LINKS NEXT
+		for (int i = 0; i < flags.Count; i++)
+		{
+			conditionals.Add((flags[i], ))
+		}
+		return;
 	}
 
 	/// <summary>
@@ -77,6 +98,7 @@ public class NewTwineParser
 		{
 			//Parse it
 			string[] splitStringToParse = stringsToParse[0].Split(' ');
+
 			bool changeFlagTo;
 			if (splitStringToParse[2] == "true") 
 			{
@@ -92,6 +114,64 @@ public class NewTwineParser
 		return result;
 	}
 
+	//private static string ParseHookLinks(NewDialogueNode node)
+	//{
+	//	int nestingLevel = 1;
+	//		
+	//	int startDelimiterStartIndex = node.Text.IndexOf("[");
+
+	//	int currentIndex = startDelimiterStartIndex;
+	//	while (currentIndex != node.Text.Length - 1)
+	//	{
+	//		currentIndex++;
+	//		if (node.Text[currentIndex - 1].ToString() + 
+	//			node.Text[currentIndex].ToString() == "[[")
+	//		{
+	//			nestingLevel++;
+	//		}
+	//	}
+	//	
+	//}
+
+	/// <summary>
+	/// Removes text between two specified delimiters, along with the delimiters.
+	/// </summary>
+	/// <param name="inputString">Input you want to edit on.</param>
+	/// <param name="startDelimiter">Start delimiter to start deleting at.</param>
+	/// <param name="endDelimiter">End delimiter to end deletion at.</param>
+	/// <returns>List of tuples, containing the removed text (excluding delimiters)
+	/// and the starting index of that removed text (including delimiters)</returns>
+	private static List<(string specialText, int removedStartIndex)> RemoveSpecialText(
+		string inputString, string startDelimiter, string endDelimiter)
+	{
+		List<(string specialText, int removedStartIndex)> results = 
+			new List<(string specialText, int removedStartIndex)>();
+		int startDelimiterStartIndex = inputString.IndexOf(startDelimiter);
+		while (startDelimiterStartIndex != -1)
+		{
+			//Find first set of delimiters
+			int specialTextStartIndex = 
+				startDelimiterStartIndex + startDelimiter.Length;
+
+			int specialTextEndIndex = inputString.IndexOf(endDelimiter, specialTextStartIndex + 1);
+
+			int specialTextLength = specialTextEndIndex - specialTextStartIndex;
+
+			string specialText = inputString.Substring(
+				specialTextStartIndex, specialTextLength);
+			results.Add((specialText, startDelimiterStartIndex));
+
+			inputString = inputString.Remove(
+				specialTextStartIndex - startDelimiter.Length,
+				specialText.Length +
+				startDelimiter.Length +
+				endDelimiter.Length);
+
+			startDelimiterStartIndex = inputString.IndexOf(startDelimiter);
+		}
+		return results;
+	}
+
 	/// <summary>
 	/// Removes text between two specified delimiters, along with the delimiters.
 	/// </summary>
@@ -103,32 +183,28 @@ public class NewTwineParser
 	private static List<(string specialText, int removedStartIndex)> RemoveSpecialText(
 		NewDialogueNode node, string startDelimiter, string endDelimiter)
 	{
-		List<(string specialText, int removedStartIndex)> results = 
-			new List<(string specialText, int removedStartIndex)>();
-		int startDelimiterStartIndex = node.Text.IndexOf(startDelimiter);
-		while (startDelimiterStartIndex != -1)
+		return RemoveSpecialText(node.Text, startDelimiter, endDelimiter);
+	}
+
+	/// <summary>
+	/// Returns list of removed text in node between delimiters.
+	/// </summary>
+	/// <param name="inputString">Input to edit on.</param>
+	/// <param name="startDelimiter">Start delimiter to look for.</param>
+	/// <param name="endDelimiter">End delimiter to look for.</param>
+	/// <returns>Returns list of removed text in node between 
+	/// delimiters.</returns>
+	private static List<string> GetRemovedSpecialText(
+		string inputString, string startDelimiter, string endDelimiter)
+	{
+		List<string> result = new List<string>();
+		foreach (
+			(string text, int) removedTextTuple in 
+			RemoveSpecialText(inputString, startDelimiter, endDelimiter))
 		{
-			//Find first set of delimiters
-			int specialTextStartIndex = 
-				startDelimiterStartIndex + startDelimiter.Length;
-
-			int specialTextEndIndex = node.Text.IndexOf(endDelimiter, specialTextStartIndex + 1);
-
-			int specialTextLength = specialTextEndIndex - specialTextStartIndex;
-
-			string specialText = node.Text.Substring(
-				specialTextStartIndex, specialTextLength);
-			results.Add((specialText, startDelimiterStartIndex));
-
-			node.Text = node.Text.Remove(
-				specialTextStartIndex - startDelimiter.Length,
-				specialText.Length +
-				startDelimiter.Length +
-				endDelimiter.Length);
-
-			startDelimiterStartIndex = node.Text.IndexOf(startDelimiter);
-		}
-		return results;
+			result.Add(removedTextTuple.text);
+		};
+		return result;
 	}
 
 	/// <summary>
