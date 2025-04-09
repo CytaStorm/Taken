738cbe7f577c326e98209b33f00e8334d5e68421
Jeff Chen (RIT Student) <jc7117@rit.edu>
2025-04-08 20:24:57 -0400

    wip

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -12,33 +12,33 @@ public class SceneController : MonoBehaviour
     /// All graphs in scene.
     /// </summary>
     [Header("Dialogue Components")]
-	private List<NewDialogueGraph> _graphs;
+	protected List<NewDialogueGraph> _graphs;
 
 	/// <summary>
 	///	Twine json to read from.
 	/// </summary>
-	[Tooltip("Twine JSON to read from")] [SerializeField] 
-	private TextAsset _twineJson;
+	[Tooltip("Twine JSON to read from")] [SerializeField]
+    protected TextAsset _twineJson;
 
 	/// <summary>
 	/// Does the scene start with dialogue?
 	/// </summary>
 	public bool autoStartDialogue = false;
 
-	/// <summary>
-	/// Graph used for scene as a whole.
-	/// </summary>
-	private NewDialogueGraph _sceneGraph;
+    /// <summary>
+    /// Graph used for scene as a whole.
+    /// </summary>
+    protected NewDialogueGraph _sceneGraph;
 
 	/// <summary>
 	/// All dialogue flags in scene.
 	/// </summary>
 	public List<NewDialogueFlag> DialogueFlags;
 
-	/// <summary>
-	/// All interactables in scene
-	/// </summary>
-	private GameObject[] _interactables;
+    /// <summary>
+    /// All interactables in scene
+    /// </summary>
+    protected GameObject[] _interactables;
 
 	/// <summary>
 	/// UI Manager in Scene
@@ -56,45 +56,45 @@ public class SceneController : MonoBehaviour
 	/// Destination scene.
 	/// </summary>
 	[Header("Scene Changing")]
-	[Tooltip("Destination Scene")][SerializeField] private string _destinationScene;
+	[Tooltip("Destination Scene")][SerializeField] protected string _destinationScene;
 
 	/// <summary>
 	/// Time in seconds, to fade out
 	/// </summary>
 	[Tooltip("Time, in seconds, that it takes to fade out of scene.")]
-	[SerializeField]private float _fadeOutTime;
+	[SerializeField] protected float _fadeOutTime;
 
 	/// <summary>
 	/// Is the scene currently fading out?
 	/// </summary>
 	[HideInInspector] public bool FadingOut = false;
 
-	/// <summary>
-	/// Current time of fadeout.
-	/// </summary>
-	private float _fadeOutTimer = 0f;
+    /// <summary>
+    /// Current time of fadeout.
+    /// </summary>
+    protected float _fadeOutTimer = 0f;
 
 	/// <summary>
 	/// Time, in seconds, to fade in.
 	/// </summary>
 	[Space(10)]
-	[Tooltip("Time, in seconds, to fade in.")][SerializeField] private float _fadeInTime;
+	[Tooltip("Time, in seconds, to fade in.")][SerializeField] protected float _fadeInTime;
 
 	/// <summary>
 	/// Does the scene fade in?
 	/// </summary>
 	public bool FadesIn = true;
 
-	/// <summary>
-	/// Current time of fadein.
-	/// </summary>
-	private float _fadeInTimer = 0f;
+    /// <summary>
+    /// Current time of fadein.
+    /// </summary>
+    protected float _fadeInTimer = 0f;
 
 	public delegate IEnumerator OnSceneChangeHandler(float seconds);
 	public event OnSceneChangeHandler onSceneChange;
 	#endregion
 
-	[Space(10)] [SerializeField] private Material _sallosMaterial;
+	[Space(10)] [SerializeField] protected Material _sallosMaterial;
 
 	[Header("DEBUG")] public bool DEBUG;
 
@@ -122,7 +122,7 @@ public class SceneController : MonoBehaviour
 	}
 
 	// Start is called before the first frame update
-	void Start()
+	protected void Start()
 	{
 		_sallosMaterial.SetFloat("_Dissolve_Effect", 0);
 
@@ -145,7 +145,13 @@ public class SceneController : MonoBehaviour
 			interactScript.UpdateSceneGraph.AddListener(UpdateCurrentGraph);
 		}
 
-		CreateAllDialogueFlags();
+		foreach (NewDialogueGraph graph in _graphs)
+		{
+			foreach (NewDialogueNode node in graph.Nodes)
+			{
+				CreateDialogueFlags(node);
+			}
+		}
 
 		// If scene dialogue is to be auto-implemented, set dialogue to that of first NPC
 		if (autoStartDialogue)
@@ -171,8 +177,8 @@ public class SceneController : MonoBehaviour
 		}
 	}
 
-	// Update is called once per frame
-	void Update()
+    // Update is called once per frame
+    protected void Update()
 	{
 		if (DEBUG)
 		{
@@ -204,34 +210,28 @@ public class SceneController : MonoBehaviour
 		#endregion
 	}
 
-	private void ActivateSceneDialogue()
+    protected void ActivateSceneDialogue()
 	{
 		Traverser.SetNewGraph(_sceneGraph);
 
 		//print(Traverser.CurrentGraph.StartNode.Text);
 	}
 
-	/// <summary>
-	/// Populates list of dialogue flags with every flag in every dialogue graph
-	/// in the scene
-	/// </summary>
-	private void CreateAllDialogueFlags()
+    /// <summary>
+    /// Populates list of dialogue flags
+    /// in the scene
+    /// </summary>
+	/// <param name="node">node to add flags from</param>
+    protected void CreateDialogueFlags(NewDialogueNode node)
 	{
-		foreach (NewDialogueGraph graph in _graphs)
-		{
-			foreach (NewDialogueNode node in graph.Nodes)
-			{
-				foreach (NewDialogueFlag flag in node.FlagsToChange)
-				{
-					NewDialogueFlag match = DialogueFlags.FirstOrDefault(matchFlag => matchFlag.Names == flag.Names);
-					//if flag doesn't already exist
-					if (match == default(NewDialogueFlag))
-					{
-						DialogueFlags.Add(new NewDialogueFlag(flag.Names));
-					}
-				}
-			}
-		}
+		foreach (NewDialogueFlag flag in node.FlagsToChange)
+        {
+            NewDialogueFlag match = DialogueFlags.FirstOrDefault(
+				matchFlag => matchFlag.Names.SequenceEqual(flag.Names));
+			//skip if there is a match
+            if (match != null) continue;
+            DialogueFlags.Add(new NewDialogueFlag(flag.Names));
+        }
 	}
 
 	/// <summary>
@@ -247,14 +247,14 @@ public class SceneController : MonoBehaviour
 	public bool CheckTraversal(List<NewDialogueFlag> flagsToCheck)
 	{
 		return Traverser.CheckTraversal(flagsToCheck);
-	}
+		}
 
-	/// <summary>
-	/// Update the currentGraph variable based on the npcScript the sceneManager gets an
-	/// UpdateSceneGraph call from
-	/// </summary>
-	/// <param name="npcScript">Script that the sceneManager recieved an event call from</param>
-	private void UpdateCurrentGraph(InteractableScript interactScript)
+    /// <summary>
+    /// Update the currentGraph variable based on the npcScript the sceneManager gets an
+    /// UpdateSceneGraph call from
+    /// </summary>
+    /// <param name="npcScript">Script that the sceneManager recieved an event call from</param>
+    protected void UpdateCurrentGraph(InteractableScript interactScript)
 	{
 		Traverser.SetNewGraph(interactScript.Graph);
 	}
@@ -270,6 +270,8 @@ public class SceneController : MonoBehaviour
 			}
 		}	
 	}
+
+	//private bool CheckNameListEquality()
 
 	public void NextScene()
 	{
